6a7359c996764089d21d46e59153ba60fd026f81
Ashutosh Gaur <121347667+ashutoshgaur0809@users.noreply.github.com>
2024-09-10 16:36:04 +0530

    Create diff_bw_two_arrya.py

---
+++ diff_bw_two_arrya.py
@@ -0,0 +1,32 @@
+class Solution:
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        n1 = set(nums1)
+        n2 = set(nums2)
+        # l = []
+        l1 = []
+        l2 = []
+        for i in n1:
+            if i not in n2:
+                l1.append(i)
+            
+        for i in n2:
+            if i not in n1:
+                l2.append(i)
+        
+       
+
+        return [l1,l2]
+# Example 1:
+
+# Input: nums1 = [1,2,3], nums2 = [2,4,6]
+# Output: [[1,3],[4,6]]
+# Explanation:
+# For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+# For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+# Example 2:
+
+# Input: nums1 = [1,2,3,3], nums2 = [1,1,2,2]
+# Output: [[3],[]]
+# Explanation:
+# For nums1, nums1[2] and nums1[3] are not present in nums2. Since nums1[2] == nums1[3], their value is only included once and answer[0] = [3].
+# Every integer in nums2 is present in nums1. Therefore, answer[1] = [].
